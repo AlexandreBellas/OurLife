37d2dac03a1680cdbcbf2badef2b211972f20dd6
Alexadre Batistella Bellas <alexandre.bellas@usp.br>
2018-06-09 12:06:11 -0300

    Teste

---
+++ interfaceFirebase.py
@@ -0,0 +1,12 @@
+import pyrebase
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com",
+  "serviceAccount": "path/to/serviceAccountCredentials.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+
